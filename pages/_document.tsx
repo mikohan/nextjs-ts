97f000c0f93d78d49f3d04fa59e8b3b6bc041532
mikohan <angara99@gmail.com>
2021-02-21 11:25:42 +0300

    Start add ssr styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,18 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+import { ServerStyleSheets } from '@material-ui/core';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    // Render the app and get the context of the page with collected side effects
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhancedApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
