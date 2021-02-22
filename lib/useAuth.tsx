34aa14f2e249fb16f5313f6a0cd2bfcd736d312a
mikohan <angara99@gmail.com>
2021-02-22 12:39:02 +0300

    Added signin mutation

--- lib/useAuth.tsx
+++ lib/useAuth.tsx
@@ -14,4 +14,36 @@ type AuthProps = {
   signOut: () => void;
 };
 
-const AuthContext = createContext();
+const AuthContext = createContext<Partial<AuthProps>>({});
+
+function useProvideAuth() {
+  const client = useApolloClient();
+  const router = useRouter();
+
+  const [error, setError] = useState('');
+  const { data } = useCurrentUserQuery({
+    fetchPolicy: 'network-only',
+    errorPolicy: 'ignore',
+  });
+  const user = data && data.currentUser;
+
+  // Signing in and signing up
+  const [signInMutation] = useSignInMutation();
+  const [signUpMutation] = useSignUpMutation();
+
+  const signIn = async (email, password) => {
+    try {
+      const { data } = await signInMutation({ variables: { email, password } });
+      if (data.login.token && data.login.user) {
+        localStorage.setItem('token', data.login.token);
+        client.resetStore().then(() => {
+          router.push('/');
+        });
+      } else {
+        setError('Invalid Login');
+      }
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+}
